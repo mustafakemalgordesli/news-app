ed906fb3dcb7ca66451dcb7972aece9bc82ec073
fvdime <73942727+fvdime@users.noreply.github.com>
2023-07-19 16:43:10 +0300

    navbar, footer, hero banner, slider added

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 "use client"
+import Currency from "@/components/Currency";
+import HeroBanner from "@/components/HeroBanner";
+import Slide from "@/components/Slide";
 import { Dropdown } from "flowbite-react";
 
 export default function Home() {
   return (
     <main className="">
-
+      <HeroBanner/>
+      <Slide/>
     </main>
   )
 }
