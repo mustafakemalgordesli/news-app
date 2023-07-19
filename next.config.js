d298aef7bc174827583a73da4d167fe6586d1bfe
Mustafa Kemal Gördesli <m.kemalgordesli@gmail.com>
2023-07-19 16:42:26 +0300

    blog list added and toolsection started

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'http',
+                hostname: 'http://openweathermap.org',
+                port: '',
+                pathname: 'img/wn/**',
+            },
+        ],
+    },
+}
 
 module.exports = nextConfig
