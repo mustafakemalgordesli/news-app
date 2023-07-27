6f206382b464068f21f09b0586a310a359d14179
Mustafa Kemal Gördesli <m.kemalgordesli@gmail.com>
2023-07-27 15:49:40 +0300

    slide data fetching

--- containers/HomeContainer/index.tsx
+++ containers/HomeContainer/index.tsx
@@ -1,8 +1,9 @@
+"use server"
+
 import BlogList from "@/components/BlogList"
 import ToolSection from "@/components/ToolSection"
 import HeroBanner from "@/components/HeroBanner";
 import Slide from "@/components/Slide";
-
 interface ContainerProps {
     page: number;
     category?: string;
@@ -34,16 +35,51 @@ const getData = async () => {
     return data.posts
 }
 
+const getRandomizeData = async () => {
+
+    const url = `${process.env.NEXT_PUBLIC_API_URL}/post/list?page=1&page_max=6&randomize=true`;
+
+    const res = await fetch(url, {
+        next: { revalidate: 10 }
+        // cache: "no-cache"
+    })
+
+    if (!res.ok) {
+        return null
+    }
+
+    const data = await res.json()
+
+    if (data.status === 401) {
+        return null
+    }
+
+    if (!data.success) {
+        return null
+    }
+
+    return data.posts
+}
+
 export default async function HomeContainer({ page }: ContainerProps) {
 
-    const bannerData = await getData();
+    const data1 = getData()
+
+    const data2 = getRandomizeData()
+
+    const [bannerData, slideData] = await Promise.all([data1, data2])
+
+    console.log(bannerData)
+
     return (
         <>
             {
                 bannerData && <HeroBanner slides={bannerData} />
             }
             {/* <ToolSection /> */}
-            <Slide />
+            {
+                slideData && <Slide data={slideData} />
+            }
             <BlogList page={page} />
         </>
     )
--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
     experimental: {
         nextScriptWorkers: true,
+        serverActions: true,
     },
     images: {
         domains: ["127.0.0.1"],
